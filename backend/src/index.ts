1f1ad238b954553dd023f810681df970b848d32c
Bhavya <bbhojwani31@gmail.com>
2026-03-09 02:59:19 +0530

    prepare backend for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   credentials: true,
 }));
 app.use(express.json());
